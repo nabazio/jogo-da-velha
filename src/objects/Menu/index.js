ec678186c706283c7b812b92a51777f841a211a7
Nabazio <nabazio.enry@gmail.com>
2023-03-26 20:12:12 -0300

    Criado o menu

---
+++ src/objects/Menu/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles.css';
+
+const Menu = () => (
+  <a href="#menu" className="menu">
+    <spam className="center">Menu</spam>
+  </a>
+);
+
+export default Menu;
