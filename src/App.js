2500c53f5a9e0a85438492c9798470e3f045801b
Nabazio <nabazio.enry@gmail.com>
2023-03-27 23:54:35 -0300

    Criado o object Card, apenas o JSC esta faltando o nosso CSS

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Card from './objects/Card';
 import Header from './components/Header';
 
 function App() {
   return (
     <>
       <Header />
+      <Card />
     </>
   );
 }
---
+++ src/objects/Card/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './styles.css';
+
+const Card = () => <article className="card">Card</article>;
+
+export default Card;
