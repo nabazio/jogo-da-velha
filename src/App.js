f5fa4b8e986173adc2fc02c9a8b38a8b285c8203
Nabazio <nabazio.enry@gmail.com>
2023-03-29 14:51:42 -0300

    Criou o Object Label

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import './App.css';
+import Label from './objects/Label';
 import Hashtag from './components/Hashtag';
 import Header from './components/Header';
 
-function App() {
-  return (
-    <>
-      <Header />
-      <Hashtag />
-    </>
-  );
-}
+const App = () => (
+  <main className="app">
+    <Header />
+    <Hashtag />
+    <Label content="Mostrar eventos" />
+  </main>
+);
 
 export default App;
---
+++ src/objects/Label/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles.css';
+
+const Label = ({ content }) => (
+  <label className="label" htmlFor="">
+    {content}
+  </label>
+);
+
+export default Label;
