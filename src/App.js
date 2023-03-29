07206913f625c8022a4bc095ae11a5104dcfca9e
Nabazio <nabazio.enry@gmail.com>
2023-03-28 23:50:22 -0300

    Criamos o componente hashtag e já definimos o layout básico dele

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import React from 'react';
-import Card from './objects/Card';
+import Hashtag from './components/Hashtag';
 import Header from './components/Header';
-import Player from './objects/Player';
 
 function App() {
   return (
     <>
       <Header />
-      <Card>
-        <Player player="o" />
-        <Player player="x" />
-        <Player player="o" />
-        <Player player="x" />
-        <Player player="o" />
-        <Player player="x" />
-        <Player player="o" />
-        <Player player="x" />
-        <Player player="o" />
-        <Player player="x" />
-        <Player player="o" />
-        <Player player="x" />
-      </Card>
+      <Hashtag />
     </>
   );
 }
